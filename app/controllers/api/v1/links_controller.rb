0f96f3a745ca979396887379e1acb8bd130006b1
Noah Berman <noah@noahberman.org>
2017-02-13 18:38:40 -0700

    update read count method to incremement correctly

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -3,7 +3,12 @@ class Api::V1::LinksController < ApplicationController
   def create
     link = params["urlToSend"]
     found_link = Link.find_or_create_by(url: link)
-    found_link.update(read_count: found_link.read_count + 1)
+    reads = found_link.read_count
+    if reads.nil? || found_link.reads < 1
+      found_link.update(read_count: 1)
+    else
+      found_link.update(read_count: found_link.read_count + 1)
+    end
   end
   
 end
