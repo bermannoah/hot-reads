86a33e437344f9e54d60690dae8cce2e8fd12ac5
Noah Berman <noah@noahberman.org>
2017-02-13 19:01:13 -0700

    table ordering properly

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -1,6 +1,6 @@
 class ReadsController < ApplicationController
   def index
     now = Time.now
-    @links = Link.where(updated_at: (now - 24.hours)..Time.now)
+    @links = Link.where(updated_at: (now - 24.hours)..Time.now).order(read_count: :DESC).limit(10)
   end
 end
