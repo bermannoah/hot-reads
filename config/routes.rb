a105fc5ac291172b1a6ef7e73d64047cd28ee13b
Noah Berman <noah@noahberman.org>
2017-02-13 18:10:23 -0700

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root to: "reads#index"
   
+  
+  
   namespace :api do
     namespace :v1 do
       resources :links, only: [:create, :update]
+      post "/links", to: 'links#create'
     end
   end
 end
